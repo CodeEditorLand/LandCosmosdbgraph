2f410aaf32192abb088b7e745857cc2b557f6ff2
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:08:37 +0200

    squash!

--- Source/graph/client/graphTypes.ts
+++ Source/graph/client/graphTypes.ts
@@ -10,61 +10,72 @@
 // tslint:disable:no-reserved-keywords (This code is deprecated anyway)
 
 interface GraphResults {
-  // tslint:disable-next-line:no-any
-  fullResults: any[];
-  countUniqueVertices: number;
-  countUniqueEdges: number;
+	// tslint:disable-next-line:no-any
+	fullResults: any[];
+	countUniqueVertices: number;
+	countUniqueEdges: number;
 
-  // Limited by max
-  limitedVertices: GraphVertex[];
-  limitedEdges: GraphEdge[];
+	// Limited by max
+	limitedVertices: GraphVertex[];
+	limitedEdges: GraphEdge[];
 }
 
 interface GraphEdge {
-  id: string;
-  type: "edge";
-  outV: string;  // Edge source ID
-  inV: string;   // Edge target ID
+	id: string;
+	type: "edge";
+	outV: string; // Edge source ID
+	inV: string; // Edge target ID
 }
 
 interface GraphVertex {
-  id: string;
-  type: "vertex";
-  label: string;
-  properties: {
-    [key: string]: [
-      {
-        id: string;
-        value: string;
-      }
-    ]
-  };
+	id: string;
+	type: "vertex";
+	label: string;
+	properties: {
+		[key: string]: [
+			{
+				id: string;
+				value: string;
+			},
+		];
+	};
 }
 
 interface VertexSettingsGroup {
-  appliesToLabel: string;
-  displayProperty?: string[];
-  color?: string;
-  showLabel?: boolean;
+	appliesToLabel: string;
+	displayProperty?: string[];
+	color?: string;
+	showLabel?: boolean;
 }
 
 interface GraphSettingsGroup {
-  vertexSettings?: VertexSettingsGroup[];
+	vertexSettings?: VertexSettingsGroup[];
 }
 
 type GraphViewSettings = GraphSettingsGroup[];
 
 interface PageState {
-  query: string | undefined;
-  results: GraphResults | undefined;
-  errorMessage: string | undefined;
-  view: 'graph' | 'json';
-  isQueryRunning: boolean;
-  runningQueryId: number;
+	query: string | undefined;
+	results: GraphResults | undefined;
+	errorMessage: string | undefined;
+	view: "graph" | "json";
+	isQueryRunning: boolean;
+	runningQueryId: number;
 }
 
 // Messages that are sent from the server to the client
-type ServerMessage = "setTitle" | "showResults" | "showQueryError" | "setPageState";
+type ServerMessage =
+	| "setTitle"
+	| "showResults"
+	| "showQueryError"
+	| "setPageState";
 
 // Messages that are sent from the client to the server
-type ClientMessage = "getPageState" | "getTitle" | "getPageState" | "query" | "setQuery" | "setView" | "log";
+type ClientMessage =
+	| "getPageState"
+	| "getTitle"
+	| "getPageState"
+	| "query"
+	| "setQuery"
+	| "setView"
+	| "log";
