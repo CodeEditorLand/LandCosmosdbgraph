f80328366eceedf23a99e309e5a6c16319bbb165
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-27 06:01:23 +0200

    squash!

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,25 +5,36 @@
 
 // tslint:disable:no-implicit-dependencies (this allows the use of dev dependencies)
 
-import * as fse from 'fs-extra';
-import * as gulp from 'gulp';
-import * as path from 'path';
+import * as fse from "fs-extra";
+import * as gulp from "gulp";
+import * as path from "path";
 import { Stream } from "stream";
-import { gulp_installAzureAccount, gulp_installVSCodeExtension, gulp_webpack } from 'vscode-azureextensiondev';
+import {
+	gulp_installAzureAccount,
+	gulp_installVSCodeExtension,
+	gulp_webpack,
+} from "vscode-azureextensiondev";
 
 async function prepareForWebpack(): Promise<void> {
-    const mainJsPath: string = path.join(__dirname, 'main.js');
-    let contents: string = (await fse.readFile(mainJsPath)).toString();
-    contents = contents
-        .replace('out/src/extension', 'dist/extension.bundle')
-        .replace(', true /* ignoreBundle */', '');
-    await fse.writeFile(mainJsPath, contents);
+	const mainJsPath: string = path.join(__dirname, "main.js");
+	let contents: string = (await fse.readFile(mainJsPath)).toString();
+	contents = contents
+		.replace("out/src/extension", "dist/extension.bundle")
+		.replace(", true /* ignoreBundle */", "");
+	await fse.writeFile(mainJsPath, contents);
 }
 
 function gulp_installCosmosDBExtension(): Promise<void> | Stream {
-    return gulp_installVSCodeExtension('ms-azuretools', 'vscode-cosmosdb');
+	return gulp_installVSCodeExtension("ms-azuretools", "vscode-cosmosdb");
 }
 
-exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
-exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
-exports.preTest = gulp.series(gulp_installAzureAccount, gulp_installCosmosDBExtension);
+exports["webpack-dev"] = gulp.series(prepareForWebpack, () =>
+	gulp_webpack("development")
+);
+exports["webpack-prod"] = gulp.series(prepareForWebpack, () =>
+	gulp_webpack("production")
+);
+exports.preTest = gulp.series(
+	gulp_installAzureAccount,
+	gulp_installCosmosDBExtension
+);
